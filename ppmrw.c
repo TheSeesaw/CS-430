642f1ce1c683b069015fa55ec5a2e64d330d928d
seesaw <seesaw.schindele@gmail.com>
2017-09-14 19:54:26 -0700

    update variable names for readability

--- ppmrw.c
+++ ppmrw.c
@@ -11,7 +11,9 @@ typedef struct Pixel {
 } Pixel;
 
 // declare variables
-int p, t, w, trg, width, height, max_val;
+// NOTE: p = dummy char variable, t = input file ppm type,
+//       w = dummy char variable for traversing white space
+int p, t, w, target_ppm_type, width, height, max_color_value;
 Pixel* pixmap1d;
 
 // convert to p3 function
@@ -19,48 +21,48 @@ Pixel* pixmap1d;
 void convertP3(char* outfile) {
 	// detect which file type is being converted to P3
 	// writing ASCII data
-	FILE* fho = fopen(outfile, "w");
+	FILE* file_handle_out = fopen(outfile, "w");
 	if (t == 3) {
 		// copy header data
 		unsigned char space = ' ';
 		// write magic number
-		fwrite(&p, sizeof(char), 1, fho);
-		fwrite(&t, sizeof(char), 1, fho);
-		fwrite(&space, sizeof(char), 1, fho);
+		fwrite(&p, sizeof(char), 1, file_handle_out);
+		fwrite(&t, sizeof(char), 1, file_handle_out);
+		fwrite(&space, sizeof(char), 1, file_handle_out);
 		// write width
-		fwrite(&width, sizeof(char), 1, fho);
-		fwrite(&space, sizeof(char), 1, fho);
+		fwrite(&width, sizeof(char), 1, file_handle_out);
+		fwrite(&space, sizeof(char), 1, file_handle_out);
 		// write height
-		fwrite(&height, sizeof(char), 1, fho);
-		fwrite(&space, sizeof(char), 1, fho);
+		fwrite(&height, sizeof(char), 1, file_handle_out);
+		fwrite(&space, sizeof(char), 1, file_handle_out);
 		// write maxval
-		fwrite(&max_val, sizeof(char), 1, fho);
-		fwrite(&space, sizeof(char), 1, fho);
+		fwrite(&max_color_value, sizeof(char), 1, file_handle_out);
+		fwrite(&space, sizeof(char), 1, file_handle_out);
 		// write raster
-		fwrite(&pixmap1d, sizeof(Pixel), width*height, fho);
+		fwrite(&pixmap1d, sizeof(Pixel), width*height, file_handle_out);
 		// close file
-		fclose(fho);
+		fclose(file_handle_out);
 		// done
 		exit(0);
 	}
 	/*
 	else {
 	// convert to P6 (ASCII to binary)
-	FILE* fho = fopen(argv[3], "wb");
+	FILE* file_handle_out = fopen(argv[3], "wb");
 	// copy header data
 	// write magic number
-	fwrite(p, sizeof(char), 1, fho);
-	fwrite(t, sizeof(char), 1, fho);
-	fwrite(space, sizeof(char), 1, fho);
+	fwrite(p, sizeof(char), 1, file_handle_out);
+	fwrite(t, sizeof(char), 1, file_handle_out);
+	fwrite(space, sizeof(char), 1, file_handle_out);
 	// write width
-	fwrite(width, sizeof(char), 1, fho);
-	fwrite(space, sizeof(char), 1, fho);
+	fwrite(width, sizeof(char), 1, file_handle_out);
+	fwrite(space, sizeof(char), 1, file_handle_out);
 	// write height
-	fwrite(height, sizeof(char), 1, fho);
-	fwrite(space, sizeof(char), 1, fho);
+	fwrite(height, sizeof(char), 1, file_handle_out);
+	fwrite(space, sizeof(char), 1, file_handle_out);
 	// write maxval
-	fwrite(max_val, sizeof(char), 1, fho);
-	fwrite(space, sizeof(char), 1, fho);
+	fwrite(max_color_value, sizeof(char), 1, file_handle_out);
+	fwrite(space, sizeof(char), 1, file_handle_out);
 	// write raster
 	}
 	*/
@@ -77,68 +79,68 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Error reading or writing file.");
 		return 1;
 	}
-	// store the target file type in trg
-	trg = atoi(argv[1]);
+	// store the target file type in target_ppm_type
+	target_ppm_type = atoi(argv[1]);
 	// always read binary when working on windows
-	FILE* fhi = fopen(argv[2], "rb"); // open file for reading
+	FILE* file_handle_in = fopen(argv[2], "rb"); // open file for reading
 	// check file header for type
 	// read in file header
 	// store first character in dummy variable
-	p = fgetc(fhi);
+	p = fgetc(file_handle_in);
 	// store MAGIC NUMBER in t
-	t = fgetc(fhi);
+	t = fgetc(file_handle_in);
 	// travel through whitespace until a new character is reached
-	w = fgetc(fhi);
+	w = fgetc(file_handle_in);
 	while (w == ' ' || w == '\n') {
 		printf("banana");
 		if (w == '#') {
 			while (w != '\n') {
-				w = fgetc(fhi);
+				w = fgetc(file_handle_in);
 			}
  		}
-		w = fgetc(fhi);
+		w = fgetc(file_handle_in);
 	}
 	// next character will be file width
 	width = w;
 	// repeat travel process untill height value is reached
-	w = fgetc(fhi);
+	w = fgetc(file_handle_in);
 	while (w == ' ' || w == '\n') {
 		if (w == '#') {
 			while (w != '\n') {
-				w = fgetc(fhi);
+				w = fgetc(file_handle_in);
 			}
 		}
-		w = fgetc(fhi);
+		w = fgetc(file_handle_in);
 	}
 	height = w;
 	// repeat travel process until maxval character is reached
-	w = fgetc(fhi);
+	w = fgetc(file_handle_in);
 	while (w == ' ' || w == '\n') {
-		w = fgetc(fhi);
+		w = fgetc(file_handle_in);
 		if (w == '#') {
 			while (w != '\n') {
-				w = fgetc(fhi);
+				w = fgetc(file_handle_in);
 			}
 		}
 	}
-	max_val = w;
-	if (max_val > 255) {
+	max_color_value = w;
+	if (max_color_value > 255) {
 		fprintf(stderr, "Error: file is not on a 255 color scale.");
 		return 1;
 	}
 	// repeat travel process until pixel data is reached
-	w = fgetc(fhi);
+	w = fgetc(file_handle_in);
 	while (w == ' ' || w == '\n') {
 		if (w == '#') {
 			while (w != '\n') {
-				w = fgetc(fhi);
+				w = fgetc(file_handle_in);
 			}
 		}
-		w = fgetc(fhi);
+		w = fgetc(file_handle_in);
 	}
 	// w will be the first byte of pixel data
 	// unget the first character
-	w = ungetc(w, fhi);
+	w = ungetc(w, file_handle_in);
 	// allocate memory based on file size
 	pixmap1d = malloc(sizeof(Pixel)*width*height);
 	// read data into buffer
@@ -149,7 +151,7 @@ int main(int argc, char *argv[]) {
 		int g_value;
 		int b_value;
 		for (int i = 0; i < (width*height); i += 1) {
-			fscanf(fhi, "%d" "%d" "%d", &r_value, &g_value, &b_value);
+			fscanf(file_handle_in, "%d" "%d" "%d", &r_value, &g_value, &b_value);
 			pixmap1d[i].r = r_value;
 			pixmap1d[i].g = g_value;
 			pixmap1d[i].b = b_value;
@@ -162,7 +164,7 @@ int main(int argc, char *argv[]) {
 		int g_value;
 		int b_value;
 		for (int i = 0; i < (width*height); i += 1) {
-			fscanf(fhi, "%d" "%d" "%d", &r_value, &g_value, &b_value);
+			fscanf(file_handle_in, "%d" "%d" "%d", &r_value, &g_value, &b_value);
 			pixmap1d[i].r = r_value;
 			pixmap1d[i].g = g_value;
 			pixmap1d[i].b = b_value;
@@ -172,7 +174,7 @@ int main(int argc, char *argv[]) {
 	// jump to appropriate routine
 	// argument supplied in command line determines the format to be converted TO
 	// outfile is passed into function
-	switch (trg) {
+	switch (target_ppm_type) {
 	case 3:
 		convertP3(argv[3]);
 		break;
