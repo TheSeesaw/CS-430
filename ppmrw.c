46d00dabc0aea3c96ff77efac59d87b1e05d0346
TheSeesaw <seesaw.schindele@gmail.com>
2017-09-14 21:15:26 -0700

    complete P3 file read

--- ppmrw.c
+++ ppmrw.c
@@ -14,11 +14,33 @@ typedef struct Pixel {
 // NOTE: p = dummy char variable, t = input file ppm type,
 //       w = dummy char variable for traversing white space
 int p, t, w, target_ppm_type, width, height, max_color_value;
-Pixel* pixmap1d;
+Pixel* pixmap1d; // one dimensional array to hold pixel data
 
+// Function traverses white space until a character that is not a space, newline,
+// or a comment line
+// Params - file_to_read: a pointer to a file handle to be read
+// Returns - NOTHING
+void traverse_whitespace(FILE* file_to_read) {
+	// Initialize with the first character in the file
+	int current_char = fgetc(file_to_read);
+	// Traverse the file, ignoring whitespace and newlines
+	while (current_char == ' ' || current_char == '\n' || current_char == '#') {
+		// If hash, skip the entire line
+		if (current_char == '#') {
+			current_char = fgetc(file_to_read); // Initialize loop with next char
+			while (current_char != '\n') { // While not newline
+				current_char = fgetc(file_to_read);
+				// current_char now holds newline, break out of loop
+			}
+		}
+		current_char = fgetc(file_to_read); // Get the next char
+	}
+	// Unget the last non-whitespace character
+	ungetc(current_char, file_to_read);
+}
 // Function converts either P3 or P6 type ppm files to a P3 type ppm file
 // Params - outfile: the new file to write to
-// Returns - Nothing
+// Returns - NOTHING
 void convertP3(char* outfile) {
 	// detect which file type is being converted to P3
 	// writing ASCII data
@@ -89,61 +111,29 @@ int main(int argc, char *argv[]) {
 	FILE* file_handle_in = fopen(argv[2], "rb"); // open file for reading
 	// read in file header
 	p = fgetc(file_handle_in); // store first character in dummy variable
-	t = fgetc(file_handle_in); // store MAGIC NUMBER in t
-	// travel through whitespace until a new character is reached
-	// NOTE: fgetc advances the cursor each time it grabs a character
-	// TODO: factor out traversal function
-	w = fgetc(file_handle_in);
-	while (w == ' ' || w == '\n') {
-		printf("whitespace\n");
-		if (w == '#') {
-			while (w != '\n') {
-				w = fgetc(file_handle_in);
-			}
- 		}
-		w = fgetc(file_handle_in);
-	}
-	// next character will be file width
-	width = w;
-	// repeat travel process untill height value is reached
-	w = fgetc(file_handle_in);
-	while (w == ' ' || w == '\n') {
-		if (w == '#') {
-			while (w != '\n') {
-				w = fgetc(file_handle_in);
-			}
-		}
-		w = fgetc(file_handle_in);
-	}
-	height = w;
-	// repeat travel process until maxval character is reached
-	w = fgetc(file_handle_in);
-	while (w == ' ' || w == '\n') {
-		w = fgetc(file_handle_in);
-		if (w == '#') {
-			while (w != '\n') {
-				w = fgetc(file_handle_in);
-			}
-		}
-	}
-	max_color_value = w;
+	fscanf(file_handle_in, "%d", &t); // store MAGIC NUMBER in t
+	printf("Input file type: %d\n", t);
+	// traverse white space until width value is reached
+	traverse_whitespace(file_handle_in);
+	// store the width value
+	fscanf(file_handle_in, "%d", &width);
+	printf("Image width: %d\n", width);
+	// traverse white space until height value is reached
+	traverse_whitespace(file_handle_in);
+	// store the height value
+	fscanf(file_handle_in, "%d", &height);
+	printf("Image height: %d\n", height);
+	// traverse white space until color max val is reached
+	traverse_whitespace(file_handle_in);
+	// store the max color value
+	fscanf(file_handle_in, "%d", &max_color_value);
+	printf("Max color value: %d\n", max_color_value);
 	if (max_color_value > 255) {
 		fprintf(stderr, "Error: file is not on a 255 color scale.");
 		return 1;
 	}
-	// repeat travel process until pixel data is reached
-	w = fgetc(file_handle_in);
-	while (w == ' ' || w == '\n') {
-		if (w == '#') {
-			while (w != '\n') {
-				w = fgetc(file_handle_in);
-			}
-		}
-		w = fgetc(file_handle_in);
-	}
-	// w will be the first byte of pixel data
-	// unget the first character
-	w = ungetc(w, file_handle_in);
+	// traverse white space until pixel data is reached
+	traverse_whitespace(file_handle_in);
 	// allocate memory based on file size
 	pixmap1d = malloc(sizeof(Pixel)*width*height);
 	// declare variables for color values
@@ -152,12 +142,12 @@ int main(int argc, char *argv[]) {
 	switch (t) {
 		case 3:
 			// P3, data stored in ASCII, rgb
-
 			for (int i = 0; i < (width*height); i += 1) {
 				fscanf(file_handle_in, "%d" "%d" "%d", &r_value, &g_value, &b_value);
 				pixmap1d[i].r = r_value;
 				pixmap1d[i].g = g_value;
 				pixmap1d[i].b = b_value;
+				printf("Red value: %d, green value: %d, blue value: %d\n", pixmap1d[i].r, pixmap1d[i].g, pixmap1d[i].b);
 			}
 			break;
 		case 6:
@@ -167,11 +157,13 @@ int main(int argc, char *argv[]) {
 				pixmap1d[i].r = r_value;
 				pixmap1d[i].g = g_value;
 				pixmap1d[i].b = b_value;
+				printf("Red value: %d, green value: %d, blue value: %d\n", pixmap1d[i].r, pixmap1d[i].g, pixmap1d[i].b);
 			}
 	}
 	// jump to appropriate routine
 	// argument supplied in command line determines the format to be converted TO
 	// outfile is passed into function
+	/*
 	switch (target_ppm_type) {
 	case 3:
 		convertP3(argv[3]);
@@ -182,6 +174,8 @@ int main(int argc, char *argv[]) {
 	default:
 		fprintf(stderr, "Error: Invalid file.");
 	}
-
-	return 1;
+	*/
+	// close the input file before exiting
+	fclose(file_handle_in);
+	return 0;
 }
